185b4b0e5e6a236c281cb84010be924f544ecf43
Musstavo <mohdtantour@gmail.com>
2026-01-31 16:02:34 +0300

    meh

--- test.cpp
+++ test.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+
 int main() {
-  std::cout << "C++ is ready for the AVM project!" << std::endl;
+  std::cout << "Enter a number: ";
+  int x{};
+  int y{};
+  std::cin >> x >> y;
+  std::cout << "Number chosen was: " << x << y;
   return 0;
 }
